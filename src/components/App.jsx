c030d197adacdc0ecac45852606fbdaa7df1656f
Marcos Bueno <marcos_vbp@outlook.com>
2025-05-08 19:47:36 -0300

    feat: add dinamic pagination and limit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,27 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
+  const [news, setNews] = useState([{}])
+  const [page, setPage] = useState(1)
+  const [limit, setLimit] = useState(1)
+
+  const handleNews = (value) => {
+    setNews(value)
+  }
+
+  const handlePage = (value) => {
+    setPage(value)
+  }
+
+  const handleLimit = (value) => {
+    setLimit(value)
+  }
+
   useEffect(() => {
-    const text = document.getElementById('api-content')
     const API_KEY = 'O0jp6kdnPaGPOfJxI1NFDlzjAHiGavEG5ZUVMEeC'
-    const apiUrl = `https://api.thenewsapi.com/v1/news/all?api_token=${API_KEY}&search=UTFPR&limit=1`
+    const apiUrl = `https://api.thenewsapi.com/v1/news/all?api_token=${API_KEY}&search=UTFPR&limit=${limit}&page=${page}`
+
     fetch(apiUrl)
       .then(response => {
         if (!response.ok) {
@@ -14,13 +30,13 @@ function App() {
         return response.json()
       })
       .then(responseJSON => {
-        console.log(responseJSON.data)
-        text.innerHTML = responseJSON['data'][0]['title']
+        const newsArray = responseJSON.data
+        handleNews(newsArray)
       })
       .catch(error => {
         console.error('Erro', error)
       })
-  }, [])
+  }, [limit, page])
 
   return (
     <>
@@ -29,10 +45,29 @@ function App() {
         <h1>
           Bem vindo à um site muito melhor que o G1 S2
         </h1>
+        <label htmlFor='page'>Page</label>
+        <input
+          type='number'
+          id='page'
+          value={page}
+          onChange={(e) => handlePage(e.target.value)}
+        />
+        <br />
+        <label htmlFor='limit'>Limit</label>
+        <input
+          type='number'
+          id='limit'
+          value={limit}
+          onChange={(e) => handleLimit(e.target.value)}
+        />
         <p>
           Conteudo da API
         </p>
-        <span id='api-content'></span>
+        <ul id='api-content'>
+          {news.map((item, index) => (
+            <li key={index}>{item.title}</li>
+          ))}
+        </ul>
       </div>
     </>
   )
