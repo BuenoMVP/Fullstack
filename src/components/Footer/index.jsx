1f7fd1056cb273aff84bdb7ed87a117c0921b7d1
gustavo-bueno <gustavobcarvalho22@gmail.com>
2025-05-09 00:02:56 -0300

    feat: create footer component

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,31 @@
+import { Box, Container, Typography } from "@mui/material";
+
+const content = {
+  en: "© 2025 UTFPR News. All rights reserved.",
+  pt: "© 2025 UTFPR News. Todos os direitos reservados.",
+};
+
+const styles = {
+  footer: {
+    width: "100%",
+    py: 3,
+    backgroundColor: "white",
+    color: (theme) => theme.palette.grey[900],
+  },
+};
+
+const language = "pt";
+
+function Footer() {
+  return (
+    <Box component="footer" sx={styles.footer}>
+      <Container maxWidth="xl">
+        <Typography variant="body2" align="center">
+          {content[language]}
+        </Typography>
+      </Container>
+    </Box>
+  );
+}
+
+export default Footer;
