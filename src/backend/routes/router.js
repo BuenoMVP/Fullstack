a458ee184c96050b4f6ed9b8395aeb18b2816065
Marcos Bueno <marcos_vbp@outlook.com>
2025-06-28 16:13:50 -0300

    feat: add news CRUD and table

--- src/backend/routes/router.js
+++ src/backend/routes/router.js
@@ -1,9 +1,12 @@
 import express from 'express'
 import usersRouter from './users.js'
+import newsRouter from './news.js'
 
 const router = express.Router()
 
 router.use('/users', usersRouter)
+
+router.use('/news', newsRouter)
 
 router.get('/', (_req, res) => {
     res.send('Realize o longin')
