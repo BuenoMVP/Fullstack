449284c761da0051cac891e37c3674a26993d866
gustavo-bueno <gustavobcarvalho22@gmail.com>
2025-06-29 21:57:30 -0300

    feat: integrate news section with our api

--- frontend/src/components/NewsCard.jsx
+++ frontend/src/components/NewsCard.jsx
@@ -1,10 +1,10 @@
 import { Card, CardContent, CardMedia, Typography } from "@mui/material";
 
-function NewsCard({ title, description, image_url: imageUrl, url, date }) {
+function NewsCard({ titulo, descricao, imagem, link, data }) {
   return (
       <Card 
         component="a"
-        href={url}
+        href={link}
         target="_blank"
         rel="noopener noreferrer"
         sx={{
@@ -20,18 +20,18 @@ function NewsCard({ title, description, image_url: imageUrl, url, date }) {
         <CardMedia
           component="img"
           height="200"
-          image={imageUrl}
-          alt={title}
+          image={imagem}
+          alt={titulo}
         />
         <CardContent sx={{ flexGrow: 1 }}>
           <Typography gutterBottom variant="h5" component="h2">
-            {title}
+            {titulo}
           </Typography>
           <Typography variant="body2" color="text.secondary">
-            {date}
+            {data}
           </Typography>
           <Typography>
-            {description}
+            {descricao}
           </Typography>
         </CardContent>
       </Card>
