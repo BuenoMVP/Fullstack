16daedf462adc70be293dc24f1f3398b2f192233
gustavo-bueno <gustavobcarvalho22@gmail.com>
2025-06-29 21:58:38 -0300

    feat: add compression for static files on vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,32 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import compression from 'vite-plugin-compression'
 import process from 'node:process'
 
 // https://vite.dev/config/
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, process.cwd())
   return {
-    plugins: [react()],
+    plugins: [
+      react(),
+      compression({
+        algorithm: 'gzip',
+        ext: '.gz',
+        threshold: 10240,
+      }),
+    ],
     server: {
       port: parseInt(env.VITE_PORT) || 3000,
+    },
+    build: {
+      minify: 'esbuild',
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            vendor: ['react', 'react-dom'],
+          },
+        },
+      },
     },
   }
 })
